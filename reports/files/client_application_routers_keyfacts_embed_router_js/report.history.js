f1139ac3cd1afc161885ebf8b94ebeccad083c92
dschonfe <schonfeld.david@gmail.com>
2014-05-04 16:45:35 +0200

    files addded

---
+++ reports/files/client_application_routers_keyfacts_embed_router_js/report.history.js
@@ -0,0 +1,1 @@
+__history = [{"date":"Sun, 04 May 2014 14:44:51 GMT","sloc":51,"lloc":11,"functions":3,"deliveredBugs":0.10684511669647444,"maintainability":77.18917957395924,"lintErrors":5,"difficulty":6}]
