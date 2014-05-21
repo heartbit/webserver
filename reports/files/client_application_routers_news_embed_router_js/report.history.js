20fc0fce1ebea76125db790e92b143a687d1074c
dschonfe <schonfeld.david@gmail.com>
2014-05-21 22:51:03 +0200

    news restyling

--- reports/files/client_application_routers_news_embed_router_js/report.history.js
+++ reports/files/client_application_routers_news_embed_router_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Mon, 12 May 2014 18:18:05 GMT","sloc":17,"lloc":6,"functions":2,"deliveredBugs":0.06267252957521456,"maintainability":80.21731152098464,"lintErrors":0,"difficulty":7.1875}]
+__history = [{"date":"Mon, 12 May 2014 18:18:05 GMT","sloc":17,"lloc":6,"functions":2,"deliveredBugs":0.06267252957521456,"maintainability":80.21731152098464,"lintErrors":0,"difficulty":7.1875},{"date":"Wed, 21 May 2014 20:43:26 GMT","sloc":17,"lloc":6,"functions":2,"deliveredBugs":0.06267252957521456,"maintainability":80.21731152098464,"lintErrors":0,"difficulty":7.1875}]
