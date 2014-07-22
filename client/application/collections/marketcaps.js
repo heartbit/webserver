28a43e98b5c30a3e9fa94fe428e00333ed50803f
François <francois.richard12@gmail.com>
2014-07-22 04:23:36 +0200

    ij

--- client/application/collections/marketcaps.js
+++ client/application/collections/marketcaps.js
@@ -16,9 +16,8 @@ define('marketcaps', ['config', 'marketcap'], function(config, Marketcap) {
 					//FIX TEMPORAIRE pour corriger data ber non parsée (BTC) ou buguée pour l'instant (DRK)
 					if(response.currencyId=="BTC") {
 						response.totalCoin=response.totalCoin*Math.pow(10,-8);
-					}else if(response.currencyId=="DRK") {
-						response.totalCoin=response.totalCoin/2*Math.pow(10,-4);
 					}
+				
 				
 					responseFilter.push(response);
 				}
