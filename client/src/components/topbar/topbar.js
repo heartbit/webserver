7208307520d71ce8513fdcfd11212fed07b6dde9
francois.richard12@gmail.com <francois.richard12@gmail.com>
2015-09-03 18:39:47 +0200

    home page & clean useless stuff

--- client/src/components/topbar/topbar.js
+++ client/src/components/topbar/topbar.js
@@ -12,54 +12,8 @@ var Topbar = React.createClass({
           <div id="top-panel" className="col-xs-12 col-sm-10">
             <div className="row">
               <div className="col-xs-8 col-sm-4">
-                <a href="#" className="show-sidebar">
-                  <i className="fa fa-bars"></i>
-                </a>
-                <div id="search">
-                  <input type="text" placeholder="search"/>
-                  <i className="fa fa-search"></i>
-                </div>
               </div>
               <div className="col-xs-4 col-sm-8 top-panel-right">
-                <ul className="nav navbar-nav pull-right panel-menu">
-                  <li className="hidden-xs">
-                    <a href="#" className="modal-link">
-                      <i className="fa fa-lock"></i>
-                    </a>
-                  </li>
-                  <li className="dropdown">
-                    <a href="#" className="dropdown-toggle account" data-toggle="dropdown">
-                      <div className="avatar">
-                        <img src="./img/avatar.jpg" className="img-rounded" alt="avatar" />
-                      </div>
-                      <i className="fa fa-angle-down pull-right"></i>
-                      <div className="user-mini pull-right">
-                        <span className="welcome">Welcome,</span>
-                        <span>mysterious user</span>
-                      </div>
-                    </a>
-                    <ul className="dropdown-menu">
-                      <li>
-                        <a href="#">
-                          <i className="fa fa-user"></i>
-                          <span>Profile</span>
-                        </a>
-                      </li>
-                      <li>
-                        <a href="#">
-                          <i className="fa fa-cog"></i>
-                          <span>Settings</span>
-                        </a>
-                      </li>
-                      <li>
-                        <a href="#">
-                          <i className="fa fa-power-off"></i>
-                          <span>Logout</span>
-                        </a>
-                      </li>
-                    </ul>
-                  </li>
-                </ul>
               </div>
             </div>
           </div>
@@ -69,3 +23,49 @@ var Topbar = React.createClass({
   }
 });
 module.exports = Topbar;
+                // <a href="#" className="show-sidebar">
+                //   <i className="fa fa-bars"></i>
+                // </a>
+                // <div id="search">
+                //   <input type="text" placeholder="search"/>
+                //   <i className="fa fa-search"></i>
+                // </div>
+                // <ul className="nav navbar-nav pull-right panel-menu">
+                //   <li className="hidden-xs">
+                //     <a href="#" className="modal-link">
+                //       <i className="fa fa-lock"></i>
+                //     </a>
+                //   </li>
+                //   <li className="dropdown">
+                //     <a href="#" className="dropdown-toggle account" data-toggle="dropdown">
+                //       <div className="avatar">
+                //         <img src="./img/avatar.jpg" className="img-rounded" alt="avatar" />
+                //       </div>
+                //       <i className="fa fa-angle-down pull-right"></i>
+                //       <div className="user-mini pull-right">
+                //         <span className="welcome">Welcome,</span>
+                //         <span>mysterious user</span>
+                //       </div>
+                //     </a>
+                //     <ul className="dropdown-menu">
+                //       <li>
+                //         <a href="#">
+                //           <i className="fa fa-user"></i>
+                //           <span>Profile</span>
+                //         </a>
+                //       </li>
+                //       <li>
+                //         <a href="#">
+                //           <i className="fa fa-cog"></i>
+                //           <span>Settings</span>
+                //         </a>
+                //       </li>
+                //       <li>
+                //         <a href="#">
+                //           <i className="fa fa-power-off"></i>
+                //           <span>Logout</span>
+                //         </a>
+                //       </li>
+                //     </ul>
+                //   </li>
+                // </ul>
